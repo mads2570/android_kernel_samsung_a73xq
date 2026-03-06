e5cb13ea2c9d933e34995038bcfea42226bcb71f
mads2570 <qwerty2570@live.com>
2026-03-06 20:28:14 +0300

    fix: add missing definitions to xt_dscp.h

--- include/linux/netfilter/xt_dscp.h
+++ include/linux/netfilter/xt_dscp.h
@@ -5,9 +5,16 @@
 
 #define XT_DSCP_MASK	0xfc	/* 11111100 */
 #define XT_DSCP_SHIFT	2
+#define XT_DSCP_MAX	0x3f	/* 00111111 */
 
 struct xt_dscp_info {
 	__u8 dscp;
+	__u8 invert;
+};
+
+struct xt_tos_match_info {
+	__u8 tos_value;
+	__u8 tos_mask;
 	__u8 invert;
 };
 
